70bb5d916d362ea20dd8d36fdef9b1e8acd5dc30
Mikerah <mikerah@hashcloak.com>
2025-01-15 15:50:50 -0500

    Skeleton stoffel cli

---
+++ src/main.rs
@@ -0,0 +1,140 @@
+use clap::{Parser, Subcommand};
+
+/// Stoffel - A command line tool
+#[derive(Parser, Debug)]
+#[command(
+    name = "stoffel",
+    author,
+    version,
+    about,
+    long_about = "Stoffel is a framework for building MPC applications"
+)]
+struct Cli {
+    /// Optional path to operate on
+    #[arg(short, long)]
+    path: Option<String>,
+
+    /// Enable verbose output
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Initialize a new stoffel project
+    Init {
+        #[command(subcommand)]
+        kind: InitCommands,
+    },
+    /// Check the status of the current project
+    Status,
+}
+
+#[derive(Subcommand, Debug)]
+enum InitCommands {
+
+    /// Initialize a new Stoffel project with smart contracts for a specific chain
+    Chain {
+        /// Name of the chain
+        #[arg(short, long)]
+        name: Option<String>,
+    },
+
+    /// Initialize a new Stoffel project over a specific computational domain
+    Domain {
+        /// Name of the domain
+        #[arg(short, long)]
+        name: Option<u64>,
+    }
+}
+
+fn display_honeybadger() {
+    println!(r#"
+    Stoffel is a honeybadger that helps you build MPC applications.
+    Honeybadgers are a fearless breed of animals that are known for their tenacity and resilience.
+    MPC is a powerful tool that allows you to build applications that are secure, scalable, and efficient. Just like Stoffel.
+
+                                                                                                                                                  
+                                                   @    .                                           
+                                                @@@@@@@@@*@@                                        
+                                              @@+-@   --@@@@                                        
+                                          @@@@ --------------@@@                                    
+                                     @@@@   -----------------@@@@@                                  
+                                 @@@@  ---------------------------@@@@                              
+                              *@@@  :::::::::::::::::::-------------- @@@                           
+                            @@@  :::::::::::::::::::::::::::------------ @@                         
+                          @@@  :::::::::::::::::::::::::::::::::-*----%--- @@                       
+                         @@  :+=%%%%%%%%@%@@@:::::::::::::@::::=%%@%@-@%%%@- @@                     
+                       @@:%%%%%%%%%%%%#########%::::::::##%%%%%%%%%%%%%%%%%%%@ @@.                  
+                      @@-%%%%%%%%%################@:@#########################%@@@                  
+                     @:#%%#######################################################@@                 
+                   @@:#############@@#############################%@##############:@                
+                  @@:##################@#######################@###################@@@@##@@         
+           @@##@@@@:######################@#################@*######################@###@#@         
+          @@#@####:########################*@#############@++##########################%%@@@.       
+          @#%%%#############################+@###########@++##########################@%%%@@        
+          @#%%%%##########@@=====@@@@@######++%#####****%@+*#####@@@@@@====@@#########%%%%%@        
+         #@#%%%%@##########@=====@  @@@@@@###+%*********%@***#@% @@@@@=====@#########@#@%%%@        
+          @#%%@#############@....@%%%%%%@@#**@***********#***@@  %%%%@....@############@%%@@        
+          @:%%%@###########*#@....@%####.-*******##@@@##*****@.%%%@@@....@@*############%#@@        
+          @##%@##############*++@@..@@@...*****++++++++++****+  @@@..@@++*############%%%:@         
+           @#%%%#################++#####@.*********@@@******* @*****#+++##############%%:@%         
+           @@#%%########################****@%%%%%%%%%%%%%%@********###################:@@          
+            @@%:#########%%@@@%%%#######***@@@@   .        @@*******####%%@@%%########:@@           
+             @@:##############@##########***%%%@%%%%%%%%%%%%********##%#@@############@:@           
+             @@##%#############@##########***%%%#%%%%%%@%%%********####@=#############:@@@          
+             @:%%##############@=#############**@#%%%%#@*********#####@=##############:@            
+            .@%%@#%%############@=#######@#########@%################@=#############%##@+           
+            @@#@:#%%#############==@###############################%=@@#############%#:@            
+               @:%%%%#############@=.@%#######%%%%%%%%%%%#######%%@.=@#############%%%:@            
+               @@%%%%%%%###########=..@=.@@@@@@@@-....@@@@@@@@@==@..@##############:%:@@            
+               @@%@:%%%%%###########=..==......................=@.==###########%:#:@@:@             
+                @%@@:%%%%%%#########@..@=...@...=@....@=..@...==...###########%:%:@@@@              
+                @@  @:%%%%%%%%########..=..@..................=@.@#######%%#%:@@:@@                 
+                     @@:%@::%%%%#######@-=@.................=.=@#######%%%%%:@@@@                   
+                       @@:@@@::%%%%##########@@@@@@@@@@@@@%##@#######%%:%%:@@                       
+                          @@@@@@::%@:%#############################%::@@%@@                         
+                                 @@:@@@::######################%:%:@@ @:@                           
+                                    @@ @@@:%%%%############%%%:*@@                                  
+                                           @@::@:%%%%%%:%%:@%:@                                     
+                                             @@@@@:::@@+@@@@@                                       
+                                                    @ +                                             
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+
+
+"#);
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // If no subcommand is provided, show the honeybadger
+    if std::env::args().len() == 1 {
+        display_honeybadger();
+        return;
+    }
+
+    if cli.verbose {
+        println!("Running command: {:?}", cli.command);
+    }
+
+    match cli.command {
+        Commands::Init { kind } => match kind {
+            InitCommands::Chain { name } => {
+                println!("Initializing new stoffel project with smart contracts for chain {}", name.map_or(String::new(), |n| format!(" '{n}'")));
+            }
+            InitCommands::Domain { name } => {
+                println!("Initializing new stoffel project over the computational domain {}", name.map_or(String::new(), |n| format!(" '{n}'")));
+            }
+        },
+        Commands::Status => println!("Checking status..."),
+    }
+} 
